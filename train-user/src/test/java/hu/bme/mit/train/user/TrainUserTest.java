8a227a7565771917b9b7dcedda05bb2fd31b9fb3
Tadam16 <tumay.adam@gmail.com>
2022-03-07 16:22:13 +0100

    Fixed adding TrainUserTest

---
+++ train-user/src/test/java/hu/bme/mit/train/user/TrainUserTest.java
@@ -0,0 +1,17 @@
+package hu.bme.mit.train.user;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TrainUserTest {
+
+    @Before
+    public void before() {
+    }
+
+    @Test
+    public void AssertingTrue() {
+        Assert.assertTrue(true);
+    }
+}
